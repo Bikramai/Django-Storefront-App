75a868842c6efaf9a48935a37574c675caba31c7
bikramia <bikram.rhce@gmail.com>
2024-09-20 18:40:26 -0400

    Writing Views function - Playground

--- Section1-Django-Fundamentals/storefront/playground/views.py
+++ Section1-Django-Fundamentals/storefront/playground/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+# request -> response
+# reequest handler
+# action
+def say_hello(request):
+    return HttpResponse('Hello World')
+    # Pull data from db
+    # Transform 
+    # Send email 
