94c57d66d0c56f818e7c239b076c07613a991c88
bikramia <bikram.rhce@gmail.com>
2024-10-07 07:39:19 -0400

    Generic Relationships

--- Section1-Django-Fundamentals/storefront/tags/models.py
+++ Section1-Django-Fundamentals/storefront/tags/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    # What tag applied to what object
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # Type (product, video, article)
+    # ID
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    objects_id = models.PositiveBigIntegerField()
+    content_object = GenericForeignKey()
+
+
+class LikedItem(models.Model):
+    # What tag applied to what object
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # Type (product, video, article)
+    # ID
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    objects_id = models.PositiveBigIntegerField()
+    content_object = GenericForeignKey()
